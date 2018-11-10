e77e39e9fd11ee47a7d5d0662aa76b916019f780
Nuno Fidalgo <nunorfidalgo@gmail.com>
2018-11-10 19:22:27 +0000

    Add project files.

---
+++ TPWEB-Residual/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TPWEB_Residual.Startup))]
+namespace TPWEB_Residual
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
